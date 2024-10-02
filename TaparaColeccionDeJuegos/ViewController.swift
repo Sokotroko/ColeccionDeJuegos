da4abcf9d2c1d6f311e914675d755fbe4c65e17e
Niels <niels.tapara@tecsup.edu.pe>
2024-10-02 17:22:52 -0500

    Integracion de mostrar imagenes en table view principal

--- TaparaColeccionDeJuegos/ViewController.swift
+++ TaparaColeccionDeJuegos/ViewController.swift
@@ -1,19 +1,36 @@
-//
-//  ViewController.swift
-//  TaparaColeccionDeJuegos
-//
-//  Created by Niels Tapara on 2/10/24.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return juegos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        let juego = juegos[indexPath.row]
+        cell.textLabel?.text = juego.titulo
+        cell.imageView?.image = UIImage(data: (juego.imagen!))
+        return cell
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    var juegos : [Juego] = []
+    
+    override func viewWillAppear(_ animated: Bool) {
+        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        do {
+            try juegos = context.fetch(Juego.fetchRequest())
+            tableView.reloadData()
+        } catch {
+            // Manejo de errores
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
-
-
 }
 
